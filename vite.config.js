278cd39fa55faa15ec742e17539f16fdaa7dc6c5
Mohd Shakib Raza <59337530+ShakibRaza123@users.noreply.github.com>
2026-03-28 18:33:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/', // 🔥 IMPORTANT FIX
-  plugins: [
-    react(),
-    tailwindcss()
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-})
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
